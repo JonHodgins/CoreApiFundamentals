1d3eeddbe66af17bf80d95ce73c5958550aa5b2a
Jon Hodgins <19355502+JonHodgins@users.noreply.github.com>
2021-07-12 15:48:21 -0700

    Complete GET actions

---
+++ src/Data/CampProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+            CreateMap<Camp, CampModel>()
+                .ForMember(cm => cm.Venue, o => o.MapFrom(c => c.Location.VenueName));
+
+            CreateMap<Talk, TalkModel>();
+
+            CreateMap<Speaker, SpeakerModel>();
+        }
+    }
+}
